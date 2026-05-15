af7f1941b5e8ea98289e59dadc74bb2433ef3021
rhavss <puupi169@gmail.com>
2026-05-15 20:54:30 -0300

    1

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+    <head></head>
+    <body>
+        <form method="POST" action="">
+            <p><input name="email" placeholder="E-mail" type="text"></p>
+            <p><input name="senha" type="password"></p>
+            <p><a href="">Esqueceu sua senha?</a></p>
+            <p><input value="Entrar" type="button"></p>
+        </form>
+    </body>
+</html>
